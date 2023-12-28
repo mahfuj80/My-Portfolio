e4610d1354ec61ed253e635a0554ba4767554b02
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-12-28 21:04:03 +0600

    back to top component added

--- src/Components/BackToTop/BackToTop.jsx
+++ src/Components/BackToTop/BackToTop.jsx
@@ -1,4 +1,3 @@
-import { FaArrowUp } from 'react-icons/fa';
 import './BackToTop.css';
 
 const BackToTop = () => {
@@ -34,7 +33,15 @@ const BackToTop = () => {
       id="myBtn"
       title="Go to top"
     >
-      <FaArrowUp></FaArrowUp>
+      <svg
+        width="24"
+        height="24"
+        xmlns="http://www.w3.org/2000/svg"
+        fillRule="evenodd"
+        clipRule="evenodd"
+      >
+        <path d="M24 12c0 6.623-5.377 12-12 12s-12-5.377-12-12 5.377-12 12-12 12 5.377 12 12zm-1 0c0 6.071-4.929 11-11 11s-11-4.929-11-11 4.929-11 11-11 11 4.929 11 11zm-11.5-4.828l-3.763 4.608-.737-.679 5-6.101 5 6.112-.753.666-3.747-4.604v11.826h-1v-11.828z" />
+      </svg>
     </button>
   );
 };
