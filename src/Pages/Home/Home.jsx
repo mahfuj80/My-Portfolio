67f7427e6402dd51f41c8d6e102f1315712866e9
mahfuj80 <mahfujurrahman06627@gmail.com>
2024-08-29 23:41:45 +0600

    updated

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,10 @@
-import BackToTop from '../../Components/BackToTop/BackToTop';
-import Banner from '../../Components/Banner/Banner';
-import Contact from '../../Components/Contact/Contact';
-import Footer from '../../Components/Footer/Footer';
-import Nav from '../../Components/Nav/Nav';
-import Projects from '../../Components/Projects/Projects';
-import Skills from '../../Components/Skills/Skills';
+import BackToTop from "../../Components/BackToTop/BackToTop";
+import Banner from "../../Components/Banner/Banner";
+import Contact from "../../Components/Contact/Contact";
+import Footer from "../../Components/Footer/Footer";
+import Nav from "../../Components/Nav/Nav";
+import Projects from "../../Components/Projects/Projects";
+import Skills from "../../Components/Skills/Skills";
 
 const Home = () => {
   return (
